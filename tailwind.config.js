22838205233d53e16e3b08f979f7b9755dd63e4c
joerg <joerg@joergviola.de>
2022-11-16 08:15:37 +0000

    google fonts removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#4599DF',
-        secondary: '#F0B544',
-      }
+        primary: "#4599DF",
+        secondary: "#F0B544",
+      },
     },
     fontFamily: {
-      'body': ['Roboto', 'sans-serif'],
-    }      
+      body: ["sans-serif"],
+    },
   },
   variants: {},
   plugins: [],
-}
+};
